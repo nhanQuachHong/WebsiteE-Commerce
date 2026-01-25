66b606a531b949177c71d231108de5730f9671bd
Thang147429 <tranhuuthang.bd04@gmail.com>
2026-01-25 21:11:17 +0700

    show 3 product have most view in home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using WebsiteE_Commerce.Data;
 using WebsiteE_Commerce.Models;
 
 namespace WebsiteE_Commerce.Controllers;
@@ -7,14 +8,21 @@ namespace WebsiteE_Commerce.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly QuanLyHangHoaContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,  QuanLyHangHoaContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
+        ViewBag.TopProducts = _context.HangHoas
+            .OrderByDescending(p => p.SoLanXem)
+            .Take(3)
+            .ToList();
+            
         return View();
     }
 
