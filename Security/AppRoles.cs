0a604ce1c8bad0f59d26e36e03ce2ba196da264b
IT29-KNK <23130247@st.hcmuaf.edu.vn>
2026-01-26 19:11:49 +0700

    Add AppRoles class for user role definitions

---
+++ Security/AppRoles.cs
@@ -0,0 +1,9 @@
+namespace WebsiteE_Commerce.Security
+{
+    public static class AppRoles
+    {
+        public const string Admin = "Admin";
+        public const string Seller = "Seller";
+        public const string User = "User";
+    }
+}
